4f316a2691c1a7dadbaf901a68a217825ebfde9a
= <=>
2017-08-26 14:04:45 +0530

    Creating multiple Components

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
 
+import { Header } from "./components/header";
+import { Home } from "./components/home";
 class App extends React.Component{
     render(){
         return (
             <div className = 'container'>
                 <div className = 'row'>
                     <div className = 'col-xs-10 col-xs-offset-1' >
-                       <h1> Hello World </h1>
+                       <Header/>
+                    </div>
+                </div> 
+                <div className = 'row'>
+                    <div className = 'col-xs-10 col-xs-offset-1' >
+                       <Home/>
                     </div>
                 </div>            
             </div>
