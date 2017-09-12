57cf10b79b28d480adfcdb795361feae36058279
= <=>
2017-09-13 00:15:21 +0530

    basic States

--- src/app/components/home.js
+++ src/app/components/home.js
@@ -4,18 +4,21 @@ export class Home extends React.Component{
 
     constructor(props){
         super();
-        this.age = props.age;
-
+        this.state = {
+            age : props.age,
+        }
     }
     makeMeOlder(){
-        this.age += 2;
+        this.setState({
+            age: this.state.age + 2,
+        })
         console.log(this.age);
     }
     render(){
         //console.log(this.props);
         return(
             <div className = "container">
-                    Your name is {this.props.name} and your age is {this.age}<br/>
+                    Your name is {this.props.name} and your age is {this.state.age}<br/>
                 <button className='btn btn-primary' onClick = {() => this.makeMeOlder()}>Make me older!</button>
             </div>
         )
